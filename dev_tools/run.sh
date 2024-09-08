8da2f04ebbb8c6dd7324f21f34192a9b37e422db
knsrinath <nagasrinath@proton.me>
2024-09-08 08:15:39 +0530

    Added missing env variable
    This is just an example ;-)

--- dev_tools/run.sh
+++ dev_tools/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Use this script to run the API server with the environment variables set
-
+# This is just an example, you can set the environment variables in any way you like
 export MYSQL_HOST=localhost
 export MYSQL_PORT=3306
 export MYSQL_USER=user
 export MYSQL_PASSWORD=password
+export MYSQL_DATABASE=pdf_data
 export API_KEY=arebha!
 
 cd ..
